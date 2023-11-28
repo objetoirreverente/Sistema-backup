2542bd37a95a040220211c4cdae2d2e9ad31883d
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-11-27 21:56:20 -0300

    Update sist.js
    Estou adicionando a pasta do ano também

--- sist.js
+++ sist.js
@@ -10,6 +10,7 @@ const date = new Date();
 const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
 	'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
+const anoAtual = date.getFullYear();
 const mesAtual = months[date.getMonth()];
 const diaAtual = date.getDate();
 
@@ -23,7 +24,7 @@ fs.readdir(__dirname, (err, files) => {
     	for(var i = 0; i<files.length;i++){
     		if(files[i] != 'backup' && files[i] != 'sist.js' && files[i] != 'back.bat'){filesArr.push(files[i]);}
     	}
-
+        console.log(filesArr);
 	} 	
 }) 
 
@@ -31,41 +32,48 @@ fs.readdir(__dirname, (err, files) => {
 
 fs.access('./backup/', (error) =>{
 	if(error){
-		fs.mkdir('./backup/', (error) =>{		
-	    	if(error){
+    	fs.mkdir('./backup/', (error) =>{		
+        	if(error){
 				console.log(error);
 	    	}
 	    	else{
-    			fs.mkdir('./backup/' + mesAtual, (error) =>{		
+	    		fs.mkdir('./backup/' + anoAtual, (error) =>{		
 	    			if(error){
 		    			console.log(error);
 	       			}
-	       		    else{
-	     				fs.mkdir('./backup/' + mesAtual + '/' + diaAtual, (error) =>{		
-	             			if(error){
-		    	    			console.log(error);
-	       	       			}
-	                		else{
-	     		    			console.log('diretorio do mes de' + mesAtual + ' e do dia ' + diaAtual + ' criados :)');
-	     		    			filesArr.forEach(fn =>{
-                                	copy(fn, './backup/' + mesAtual + '/' + diaAtual);
-                    	        });
-	            	        }
-            	        });
-	                }
-                });
-            }
-        });
+	       			else{
+    					fs.mkdir('./backup/' + anoAtual + '/' + mesAtual, (error) =>{		
+	    					if(error){
+		    					console.log(error);
+	       					}
+	       		    		else{
+	     						fs.mkdir('./backup/' + anoAtual + '/' + mesAtual + '/' + diaAtual, (error) =>{		
+	             					if(error){
+		    	    					console.log(error);
+	       	       					}
+	                				else{
+	     		    					console.log('diretorio do mes de' + mesAtual + ' e do dia ' + diaAtual + ' criados :)');
+	     		    					filesArr.forEach(fn =>{
+                                			copy(fn, './backup/' + anoAtual + '/' + mesAtual + '/' + diaAtual);
+                    	        		});
+	            	        		}
+            	        		});
+	                		}
+                		});
+            		}
+        		});
+	  		}
+	 	});
 	}
 	else{
-    	fs.mkdir('./backup/' + mesAtual + '/' + diaAtual, (error) =>{
+    	fs.mkdir('./backup/' + anoAtual + '/' + mesAtual + '/' + diaAtual, (error) =>{
 	    	if(error){
 		    	console.log(error);
 	        }
 	        else{
 	     		console.log('diretorio do dia ' + diaAtual + ' criado :)');
 	     		filesArr.forEach(fn =>{
-                	copy(fn, './backup/' + mesAtual + '/' + diaAtual);
+                	copy(fn, './backup/' + anoAtual + '/' + mesAtual + '/' + diaAtual);
                 });
 	        }
         });
@@ -78,6 +86,7 @@ function copy(file, newPath){
 		const typeData = path.extname(file) != '.txt' ? data : data.toString();
 		const enovodir = newPath ? newPath + '/' + file : filenoext + 'copia' + path.extname(file);
 
+        console.log(data);
     	fs.writeFile(enovodir, typeData, (err) =>{
     		if (err) throw err;
     		else console.log('Backup do arquivo ' + file + ' criado')
